fb7a1e6b1f25f6b979839a93dc487d85f1a759aa
Kandera Kaverappa (KK) <kkaverappa@gmail.com>
2018-04-12 23:14:45 -0500

    package update

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 
- //module.exports = {
-  //  url: 'mongodb://Admin:test@ds135399.mlab.com:35399/kk-database'
- // };
+ module.exports = {
+    url: 'mongodb://Admin:test@ds135399.mlab.com:35399/kk-database'
+  };
   
 
-  module.exports = {
-    url: 'mongodb://localhost:27017/mymondodb'
-   };
+  //module.exports = {
+   // url: 'mongodb://localhost:27017/mymondodb'
+   //};
