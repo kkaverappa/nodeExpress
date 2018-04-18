9b68a8e62641aa6d3d403fef657bfd578b360325
Kandera Kaverappa (KK) <kkaverappa@gmail.com>
2018-04-17 22:13:50 -0500

    Ready to deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser     = require('body-parser');
 const db             = require('./config/db');
 const app            = express();
 const port = 8000;
-
+//Test
 //app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //app.use(bodyParser.raw());
